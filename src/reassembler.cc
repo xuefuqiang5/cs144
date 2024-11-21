0cafeccfeeb8b16cb892e506b0275d10a12bfaad
my_linux <2115653184@qq.com>
2024-11-21 09:01:50 +0800

    i have completted the wrap and unwrap function with no error

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,45 +4,24 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  //cout << "the insert function is working " << data <<"  will be inserted "<< "the last tag is  " << is_last_substring<< endl ;
+  
   int64_t t = first_index + data.size();
   if(is_last_substring) last_limited = max(last_limited, t);
-
   const int64_t capacity = output_.writer().available_capacity();
-  //cout << "the last_limited is :" << last_limited<< endl;
-
-  //cout << "the capacity is : "<<capacity<< "the flag is :"<< flag<< endl; 
   for(uint64_t i = 0, index = first_index; i < data.size(); i++, index++){
     if(index >= static_cast<uint64_t>(flag + capacity)) {break;}
-
     if(index >= char_map.size()) {char_map.resize(index + 1, '\0'); visited.resize(index + 1, 0);}
     if(visited[index] != 0) continue;
     char_map[index] = data[i];
     visited[index] = 1;
-    //cout <<char_map[index] << "  has been insert into char_map"<< endl;
     count++;
-    
-    
   }
-  
-
-  for(int i = flag, cap = capacity; cap > 0; cap--, i++)
-      {
-        if(i >= static_cast<int>(char_map.size()) or visited[i] == 0) break;
-        output_.writer().push(string(1, char_map[i]));
-        //cout << char_map[i]<< "  has been put into stream"<< endl;
-        flag++;
-      }
-  
+  for(int i = flag, cap = capacity; cap > 0; cap--, i++){
+    if(i >= static_cast<int>(char_map.size()) or visited[i] == 0) break;
+    output_.writer().push(string(1, char_map[i]));
+    flag++;
+    }
   if(flag >= last_limited and last_limited != -1) output_.writer().close();
-  
-  
-  /*for(int i = flag, j = this->bytes_pending(); j > 0; j--, i++){
-    if(char_map.find(i) == char_map.end()) continue;
-    cout<< "assember's Ele is :" <<i<<" "<< char_map[i].ch<< endl;
-  }
-  cout <<"the flag's value is :" << flag<< endl;*/
-  
 }
 
 uint64_t Reassembler::bytes_pending() const
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,32 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  
+
+  
+  return Wrap32 { zero_point + static_cast<uint32_t>(n)};
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+ 
+  
+  uint64_t cur_value = static_cast<uint64_t>(this->raw_value_ - zero_point.raw_value_);
+  
+  
+  if(checkpoint < cur_value) return cur_value;
+  
+  uint64_t checkpoint1 = checkpoint;
+  checkpoint1 = checkpoint1 - cur_value;
+  uint32_t low_32bit = checkpoint1;
+  uint32_t high_32bit = checkpoint1 >> 32;
+ 
+  if(low_32bit > static_cast<uint32_t>(1 << 31)) 
+  cur_value += (static_cast<uint64_t>(high_32bit + 1) << 32);
+  else cur_value += static_cast<uint64_t>(high_32bit) << 32;
+  
+
+  
+  return cur_value;
 }
